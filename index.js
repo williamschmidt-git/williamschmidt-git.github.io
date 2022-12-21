84c63b62efec980768f2a3acfe0690454a7cf657
williamschmidt-git <9williamschmidt@gmail.com>
2022-12-21 16:58:47 -0300

    changes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 $('input').on('change', function() {
   $('body').toggleClass('blue');
 });
+
+// function toogleDarkMode() {
+//   const element = document.body;
+//   element.classList.toggle("dark-mode");
+// }
